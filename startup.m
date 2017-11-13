4c3f7f86b0e1176c1e1cfaf530f61409122ec337
tdcwilliams <tdcwilliams@gmail.com>
2017-11-13 12:12:36 +0100

    delete addpaths_matlibdir; move it all into startup.m

--- startup.m
+++ startup.m
@@ -1,6 +1,23 @@
-%% MatlabLibrary files
-addpaths_matlibdir;
+function startup(matlibdir)
 
+%% Local folders
 addpath('BONE_progs');
 addpath('fig_scripts');
 addpath('PA06_scripts');
+
+%% MatlabLibrary folders
+if ~exist('matlibdir','var')
+   matlibdir   = [getenv('MATLIBDIR')];
+   if strcmp('', matlibdir)
+      matlibdir   = pwd;
+   end
+end
+matlibdir   = [matlibdir,'/'];
+
+addpath([matlibdir,'BES_progs/'   ])
+addpath([matlibdir,'GRN_progs/'   ])
+addpath([matlibdir,'CURVE_progs/' ])
+addpath([matlibdir,'GEN_progs/'   ])
+addpath([matlibdir,'OP_progs/'    ])
+addpath([matlibdir,'SF_progs/'    ])
+addpath([matlibdir,'STAT_progs/'  ])
